09e33324aeb9fc625ab008118918fa01ac0749fd
rodrigodasilva <ribeiro.rodrigodasilva>
2020-04-25 09:20:09 -0300

    Services e SOLID

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,43 @@
+import { startOfHour } from 'date-fns';
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+// Todo service deve ter um único método
+/**
+ * Dependency Inversion (SOLID) - neste caso é utilizado
+ * para receber o AppointmentRepository como parametro para utilizarmos
+ * neste service
+ */
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider }: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentIsSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate
+    );
+
+    if (findAppointmentIsSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
