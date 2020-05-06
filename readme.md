3274a041706fbad34bbd44125d0cb203aefbbb1c
rodrigodasilva <ribeiro.rodrigodasilva>
2020-05-06 17:07:04 -0300

    Configurando TypeORM

---
+++ readme.md
@@ -0,0 +1,2 @@
+> docker run --name gostack_postgres -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgres
+> docker start gostack_postgres
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('Server stated on port 3333');
+  console.log('🚀 Server stated on port 3333');
 });
