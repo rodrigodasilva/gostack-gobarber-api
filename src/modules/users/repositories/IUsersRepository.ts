5e4add0dca8220290c264386ac3e43fbc73d14cb
rodrigodasilva <ribeiro.rodrigodasilva>
2020-06-18 19:22:31 -0300

    Arquitetura e DDD - Injecao de dependencias

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,4 +1,4 @@
-import User from '../infra/typeorm/entities/Users';
+import User from '../infra/typeorm/entities/User';
 import ICreateUserDTO from '../dtos/ICreateUserDTO';
 
 export default interface IUsersRepository {
