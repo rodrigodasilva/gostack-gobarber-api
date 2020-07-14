1a53bc3c1e31508737b014234e4adb7d96372b98
rodrigodasilva <ribeiro.rodrigodasilva>
2020-07-13 22:11:06 -0300

    Agendamento: rotas e controllers

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,24 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderMonthAvalabilityController from '../controllers/ProviderMonthAvalabilityController';
+import ProviderDayAvalabilityController from '../controllers/ProviderDayAvalabilityController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerMonthAvalabilityController = new ProviderMonthAvalabilityController();
+const providerDayAvalabilityController = new ProviderDayAvalabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvalabilityController.index
+);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvalabilityController.index
+);
 
 export default providersRouter;
