d6e6697849346e5adba51f05df58df3c7a291c79
rodrigodasilva <ribeiro.rodrigodasilva>
2020-07-15 19:35:25 -0300

    Prestador de servicos: agenda do prestador

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,37 @@
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointmentsService', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository
+    );
+  });
+
+  it('should be able to list the appointments on a specific day', async () => {
+    const appointments1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user_id',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+
+    const appointments2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user_id',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    const availability = await listProviderAppointments.execute({
+      provider_id: 'provider',
+      year: 2020,
+      month: 5,
+      day: 20,
+    });
+
+    expect(availability).toEqual([appointments1, appointments2]);
+  });
+});
